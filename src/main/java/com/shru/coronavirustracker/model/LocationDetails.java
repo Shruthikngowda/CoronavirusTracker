75965695f2aad493c784b5a0e23e8330429eefb1
Shruthikngowda <shruthikngowda@gmail.com>
2021-09-23 23:13:34 +0530

    Auto run for each day; LocationDetails class introduced;

---
+++ src/main/java/com/shru/coronavirustracker/model/LocationDetails.java
@@ -0,0 +1,32 @@
+package com.shru.coronavirustracker.model;
+
+public class LocationDetails {
+
+	private String state;
+	private String country;
+	private int latestTotalCases;
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public int getLatestTotalCases() {
+		return latestTotalCases;
+	}
+	public void setLatestTotalCases(int latestTotalCases) {
+		this.latestTotalCases = latestTotalCases;
+	}
+	@Override
+	public String toString() {
+		return "LocationDetails [state=" + state + ", country=" + country + ", latestTotalCases=" + latestTotalCases
+				+ "]";
+	}
+	
+}
